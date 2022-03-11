6c59f2dbcf868123990dd902ad1e002dcd72daf4
Aditya Jain <adityaj5@illinois.edu>
2022-03-10 18:00:28 -0600

    Fix numbering

--- db/query.sql
+++ db/query.sql
@@ -1,29 +1,28 @@
 USE Taste_Then_Tell;
 
-SELECT
-    name
-From
-    Dining_Halls
-where
-    university_id in (
-        SELECT
-            university_id
-        from
-            Universities
-        where
-            name like "U%"
-    )
-Union
-SELECT
-    name
-From
-    Dining_Halls
-where
-    name like "U%";
-
-/* Select
- *
+/* SELECT
+ name
  From
  Dining_Halls
- order by
- dining_hall_id desc; */
+ where
+ university_id in (
+ SELECT
+ university_id
+ from
+ Universities
+ where
+ name like "%Illinois%"
+ )
+ Union
+ SELECT
+ name
+ From
+ Dining_Halls
+ where
+ name like "U%"; */
+Select
+    *
+From
+    Dining_Halls
+order by
+    dining_hall_id desc;
