c0a97e4b129b811ac3c9adc3cb5acb44e1174556
Aditya Jain <adityaj5@illinois.edu>
2022-03-10 21:11:59 -0600

    Add foods csv

--- db/query.sql
+++ db/query.sql
@@ -1,28 +1,29 @@
 USE Taste_Then_Tell;
 
-/* SELECT
- name
- From
- Dining_Halls
- where
- university_id in (
- SELECT
- university_id
- from
- Universities
- where
- name like "%Illinois%"
- )
- Union
- SELECT
- name
- From
- Dining_Halls
- where
- name like "U%"; */
-Select
-    *
+SELECT
+    name
 From
     Dining_Halls
-order by
-    dining_hall_id desc;
+where
+    university_id in (
+        SELECT
+            university_id
+        from
+            Universities
+        where
+            name like "U%"
+    )
+Intersect
+SELECT
+    name
+From
+    Dining_Halls
+where
+    name like "U%";
+
+/* Select
+ *
+ From
+ Dining_Halls
+ order by
+ dining_hall_id desc; */
