8d2dd7ad3dd09a51d8dc48630d767e3c077a8de9
miguelf4 <miguelf4@illinois.edu>
2022-04-03 20:01:55 -0500

    modified endpoints to use body instead of query

--- taste-then-tell/backend/src/api/reviews.js
+++ taste-then-tell/backend/src/api/reviews.js
@@ -24,7 +24,7 @@ router.get(
 router.get(
   '/food_id/',
   errorWrap(async (req, res) => {
-    const {food_id} = req.query;
+    const {food_id} = req.body;
     console.log(food_id);
 
     const QUERY = 'SELECT * from Reviews WHERE food_id = ' + food_id;
@@ -36,7 +36,7 @@ router.get(
         return res.send(err);
       }
 
-      sendSuccess(res, 'Successfully returned reviews that match + ' + food_id, results);
+      sendSuccess(res, `Successfully returned reviews that match ${food_id}`, results);
     });
   }),
 );
@@ -47,8 +47,8 @@ router.post(
   errorWrap(async (req, res) => {
     // (student_id == user_id) from the database
     // (review == feedback) from the database
-    const {student_id, food_id, dining_hall_id, rating, review} = req.query;
-    const review_id = Math.floor(Math.random() * 1000000);
+    const {student_id, food_id, dining_hall_id, rating, review} = req.body;
+    const review_id = Math.floor(Math.random() * 1000000) + 10000; //this is bad we're totally going to override old reviews by accident
 
     console.log("review_id: " + review_id + " student_id: " + student_id + " food_id: " + food_id + " dining_hall_id: " + dining_hall_id + " rating: " + rating + " review: " + review);
 
@@ -69,7 +69,7 @@ router.post(
 router.put(
   '/edit_review_text/',
   errorWrap(async (req, res) => {
-    const {review_id, review} = req.query;
+    const {review_id, review} = req.body;
     console.log("review_id: " + review_id + " review: " + review);
 
     const QUERY = `UPDATE Reviews SET feedback = \"${review}\" WHERE review_id = ${review_id}`;
@@ -79,8 +79,11 @@ router.put(
       if (err) {
         return res.send(err);
       }
+      if (results.affectedRows == 0) { //TODO: should this be a success? Find out
+        return sendSuccess(res, `No review text was edited because no review exists with review_id: ${review_id}`, results);
+      }
 
-      sendSuccess(res, 'Successfully edited the TEXT of the review with review_id: ' + review_id, results);
+      sendSuccess(res, `Successfully edited the TEXT of the review with review_id: ${review_id}`, results);
     });
   }),
 );
@@ -90,7 +93,7 @@ router.put(
 router.put(
   '/edit_review_rating/',
   errorWrap(async (req, res) => {
-    const {review_id, rating} = req.query;
+    const {review_id, rating} = req.body;
     console.log("review_id: " + review_id + " rating: " + rating);
 
     const QUERY = `UPDATE Reviews SET rating = ${rating} WHERE review_id = ${review_id}`;
@@ -99,8 +102,11 @@ router.put(
       if (err) {
         return res.send(err);
       }
+      if (results.affectedRows == 0) { //TODO: should this be a success? Find out
+        return sendSuccess(res, `No review rating was edited because no review exists with review_id: ${review_id}`, results);
+      }
 
-      sendSuccess(res, 'Successfully edited RATING of the review with review_id: ' + review_id, results);
+      sendSuccess(res, `Successfully edited RATING of the review with review_id: ${review_id}`, results);
     });
   }),
 );
@@ -109,7 +115,7 @@ router.put(
 router.delete(
   '/delete_review/',
   errorWrap(async (req, res) => {
-    const {review_id} = req.query;
+    const {review_id} = req.body;
     console.log("review_id: " + review_id);
 
     const QUERY = `DELETE FROM Reviews WHERE review_id = ${review_id}`;
@@ -118,8 +124,30 @@ router.delete(
       if (err) {
         return res.send(err);
       }
+      if (results.affectedRows == 0) {
+        return sendSuccess(res, `No review deleted because no review exists with review_id: ${review_id}`, results);
+      }
 
-      sendSuccess(res, 'Successfully deleted review with review_id: ' + review_id, results);
+      sendSuccess(res, `Successfully deleted review with review_id: ${review_id}`, results);
+    });
+  }),
+);
+
+//advanced query
+// SELECT u.name, dh.name, AVG(r.rating) as avgRating FROM Reviews r NATURAL JOIN Dining_Halls dh JOIN Universities u USING(university_id) GROUP BY dh.dining_hall_id ORDER BY avgRating DESC;
+
+/* endpoint to get the average review rating for all foods within all dining halls */
+router.get(
+  '/average_ratings/',
+  errorWrap(async (req, res) => {
+    const QUERY = 'SELECT u.name, dh.name, AVG(r.rating) as avgRating FROM Reviews r NATURAL JOIN Dining_Halls dh JOIN Universities u USING(university_id) GROUP BY dh.dining_hall_id ORDER BY avgRating DESC';
+    db.query(QUERY, (err, results) => {
+      console.log(QUERY);
+      if (err) {
+        return res.send(err);
+      }
+
+      sendSuccess(res, 'Successfully returned average rating for all foods within all dining halls', results);
     });
   }),
 );
